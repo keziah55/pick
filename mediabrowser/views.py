7fb76494d5af140706198689d8f7d2a058fcf005
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-05-18 21:45:37 +0100

    TG-47 #done search filters done

--- mediabrowser/views.py
+++ mediabrowser/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models import VisionItem, MediaSeries, Genre, Keyword, Person
 
+import re
 from pprint import pprint
 
 def index(request):
@@ -36,6 +37,17 @@ def index(request):
         
     if context.get('black_and_white', None) is not False:
         context['black_and_white_checked'] = True
+        
+    genres = {g.name:g.name.lower() in context.get('genres', []) for g in Genre.objects.all()}
+    if all([value is False for value in genres.values()]):
+        # don't allow all genres unchecked
+        for key in genres:
+            genres[key] = True
+    context['genres'] = genres
+    
+    if 'all_genre_checked' not in context:
+        all_genres = all(genres.values()) 
+        context['all_genre_checked'] = request.GET.get('all-genre-box', all_genres)
     
     return render(request, 'mediabrowser/index.html', context)
     
@@ -68,43 +80,63 @@ def _search(search_str, **kwargs) -> dict:
             filter_kwargs.pop('colour')
         elif black_and_white:
             filter_kwargs['colour'] = False
+            
+    genres = set(kwargs.get('genres', []))
+            
+    results = []
     
     # always search VisionItem by title
-    results = list(VisionItem.objects.filter(title__icontains=search_str, **filter_kwargs))
+    results = [film for film in VisionItem.objects.filter(title__icontains=search_str, **filter_kwargs)
+               if _check_include_film(film, results, genres)]
     
     if search_str:
         # only search people and keywords if given a search string
         persons = Person.objects.filter(name__icontains=search_str)
         for person in persons:
             # get person's films, applying filters
-            results += [film for film in person.stars.filter(**filter_kwargs) if film not in results]
-            results += [film for film in person.director.filter(**filter_kwargs) if film not in results]
+            results += [film for film in person.stars.filter(**filter_kwargs) 
+                        if _check_include_film(film, results, genres)]
+            results += [film for film in person.director.filter(**filter_kwargs) 
+                        if _check_include_film(film, results, genres)]
         keywords = Keyword.objects.filter(name__icontains=search_str)
         for keyword in keywords:
-            results += [film for film in keyword.visionitem_set.filter(**filter_kwargs) if film not in results]
-                    
-    ## to get all items with genre 'g'
-    ## g.VisionItem_set.all()
+            results += [film for film in keyword.visionitem_set.filter(**filter_kwargs) 
+                        if _check_include_film(film, results, genres)]
     
     # args to be substituted into the templates    
     context = {'film_list':results,
                'search_str':search_str}
     return context
 
+def _check_include_film(film, results, genres) -> bool:
+    """ 
+    Return True if `film` not already in `results` and any of its genres are in `genres` set.
+    
+    Parameters
+    ----------
+    film : VisionItem
+        Film to check
+    results : list
+        List of VisionItems
+    genres : set
+        Set of genre names (as lower case strings)
+    """
+    film_genres = set([g.name.lower() for g in film.genre.all()])
+    not_disjoint = True if len(genres)==0 else not genres.isdisjoint(film_genres)
+    return film not in results and not_disjoint
+
 def _get_context_from_request(request) -> dict:
     """ Try to get year and runtime min and max from `request` """
     context = {}
     
-    for key in _get_search_kwargs():
-        try:
-            value = request.GET[key]
-            # print(f"get '{key}' = {value}")
-        except:
-            # print(f"could not get value for '{key}'")
-            pass
-        else:
+    for key, value in request.GET.items():
+        if key in _get_search_kwargs():
             context[key] = value
-            
+        elif key.startswith('genre-'):
+            if 'genres' not in context:
+                context['genres'] = []
+            if (m:=re.match(r"genre-(?P<genre>.+)", key)) is not None:
+                context['genres'].append(m.group('genre'))
     return context
 
 def _get_search_kwarg_type_map():
