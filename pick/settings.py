ae8616bc514c511354d71000ec568249291d972e
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-11-10 16:23:42 +0000

    fix director and star list formatting

--- pick/settings.py
+++ pick/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'sortedm2m',
-    'el_pagination',
+    "sortedm2m",
+    "el_pagination",
 ]
 
 MIDDLEWARE = [
