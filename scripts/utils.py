b073b967f1cde49e4c76dac19b0c280356afad58
keziah55 <19816994+keziah55@users.noreply.github.com>
2025-04-21 18:18:55 +0100

    setup_django

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,9 @@
 Utility functions and imports that may be useful in an interactive session.
 """
 
+from pathlib import Path
+import sys
+import os
 from mediabrowser.models import VisionItem, Genre, Keyword, Person, MediaItem, VisionSeries
 
 
@@ -30,4 +33,22 @@ def search_items(s: str):
     return items
 
 
-__all__ = ["VisionItem", "Genre", "Keyword", "Person", "MediaItem", "search_items", "VisionSeries"]
+def setup_django():
+    """Set paths, import django and call `django.setup()`."""
+    sys.path.append(str(Path(__file__).parents[1]))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pick.settings")
+    import django
+
+    django.setup()
+
+
+__all__ = [
+    "VisionItem",
+    "Genre",
+    "Keyword",
+    "Person",
+    "MediaItem",
+    "search_items",
+    "VisionSeries",
+    "setup_django",
+]
